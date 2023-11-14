c5c20f083336ec5998271656601d76168a02583e
Valentyn Kovalyk <valentynkovalyk@gmail.com>
2023-11-14 20:53:37 +0200

    Homework 1

---
+++ homework1/homework1/homework1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I love C++\n\n";
+
+    std::cout << "*\n";
+    std::cout << "**\n";
+    std::cout << "* *\n";
+    std::cout << "*  *\n";
+    std::cout << "*   *\n";
+    std::cout << "*    *\n";
+    std::cout << "*     *\n";
+    std::cout << "*      *\n";
+    std::cout << "*       *\n";
+    std::cout << "*        *\n";
+    std::cout << "*         *\n";
+    std::cout << "*          *\n";
+    std::cout << "* * * * * * *\n";
+
+}
