e3c11736d8b5af8c86e38f44ff5a5cd1537d10c5
Valentyn Kovalyk <valentynkovalyk@gmail.com>
2023-11-24 19:59:52 +0200

    Homework 8

---
+++ homework8/hw8_functions.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "hw8_header_func.h"
+
+void countValue(int n, int& positiveCount, int& negativCount, int& zeroCount) {
+	for (int i = 0; i < n; ++i) {
+		int value = 0;
+		std::cout << "Enter value " << (i + 1) << ": ";
+		std::cin >> value;
+
+		if (value > 0) {
+			positiveCount++;
+		}
+		else if (value < 0) {
+			negativCount++;
+		}
+		else {
+			zeroCount++;
+		}
+	}
+}
+
+int factorialLoop(int f) {
+	if (f < 0) {
+		std::cout << "Please enter a number greater than ";
+		return 0;
+	}
+	else {
+		int nFactorial = 1;
+		for (int i = 1; i <= f; ++i) {
+			nFactorial *= i;
+		}
+		return nFactorial;
+	}
+}
+
+int fromBigToSmallLoop(int a) {
+	if (a < 0) {
+		std::cout << "Wrong number!" << std::endl;
+		return 0;
+	}
+	else {
+		for (int i = a; i >= 0; --i) {
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+int fromSmallToBigLoop(int a) {
+	if (a < 0) {
+		std::cout << "Wrong number!" << std::endl;
+		return 0;
+	}
+	else {
+		for (int i = 0; i <= a; ++i) {
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+	}
+}
